0c5f4d6db9dfa6414af2332d1595128480e86dfc
Leonard Brünings <leonard.bruenings@gradle.com>
2025-04-20 19:47:15 +0200

    Use fontawesome for feature card icons as well

--- spock-website/src/icons.ts
+++ spock-website/src/icons.ts
@@ -1,9 +1,9 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faBook, faCode, faLaptopCode, faComments, faCommentDots} from '@fortawesome/free-solid-svg-icons';
+import { faBook, faCode, faLaptopCode, faComments, faCommentDots, faReceipt, faMasksTheater, faVialCircleCheck, faTableCells, faStar, faCircleCheck} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faStackOverflow, faJava, faXTwitter, faMastodon, faBluesky } from '@fortawesome/free-brands-svg-icons';
 
 // Add icons to the library
-library.add(faGithub, faStackOverflow, faJava, faXTwitter, faMastodon, faBluesky, faBook, faCode, faLaptopCode, faComments, faCommentDots);
+library.add(faGithub, faStackOverflow, faJava, faXTwitter, faMastodon, faBluesky, faBook, faCode, faLaptopCode, faComments, faCommentDots, faReceipt, faMasksTheater, faVialCircleCheck, faTableCells, faStar, faCircleCheck);
 
 // Watch for <i> tags and replace with SVG
 dom.watch();
