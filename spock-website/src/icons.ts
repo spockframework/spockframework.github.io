a345b5aa11510f48d465c68f5b829e813b8b2072
Leonard Brünings <leonard.bruenings@gradle.com>
2025-04-20 16:56:15 +0200

    Add fontawesome svg icons

---
+++ spock-website/src/icons.ts
@@ -0,0 +1,9 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faBook, faCode, faLaptopCode, faComments, faCommentDots} from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faStackOverflow, faJava } from '@fortawesome/free-brands-svg-icons';
+
+// Add icons to the library
+library.add(faGithub, faStackOverflow, faJava, faBook, faCode, faLaptopCode, faComments, faCommentDots);
+
+// Watch for <i> tags and replace with SVG
+dom.watch();
