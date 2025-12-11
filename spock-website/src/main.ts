9af116cf666da6f75c7f204438a4d772a3d8ba4c
Leonard Brünings <leonard.bruenings@gradle.com>
2025-12-11 16:56:05 +0100

    Enhance AOS initialization to respect user preferences for reduced motion

--- spock-website/src/main.ts
+++ spock-website/src/main.ts
@@ -2,9 +2,12 @@ import './style.css'
 import './icons'
 import AOS from 'aos'
 
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+const disableAosByQuery = new URLSearchParams(window.location.search).get('disableaos') !== null;
+
 AOS.init({
     duration: 800, // Animation duration
     once: true, // Whether animation should happen only once - while scrolling down
     offset: 50, // Offset (in px) from the original trigger point
-    disable: new URLSearchParams(window.location.search).get('disableaos') !== null,
+    disable: prefersReducedMotion || disableAosByQuery,
 });
