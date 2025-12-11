e4306fe1b0ba018438fe79db66ec51bc7cbcec59
Leonard Brünings <leonard.bruenings@gradle.com>
2025-12-11 14:16:20 +0100

    Integrate AOS library for animations and update styles
    Switch from external dependency to baked-in delivery.

--- spock-website/src/main.ts
+++ spock-website/src/main.ts
@@ -1,2 +1,10 @@
 import './style.css'
 import './icons'
+import AOS from 'aos'
+
+AOS.init({
+    duration: 800, // Animation duration
+    once: true, // Whether animation should happen only once - while scrolling down
+    offset: 50, // Offset (in px) from the original trigger point
+    disable: new URLSearchParams(window.location.search).get('disableaos') !== null,
+});
